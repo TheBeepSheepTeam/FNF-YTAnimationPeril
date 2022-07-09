a4f40687c289d959bf723e4cfb0158bf5377f196
Parsellol <87391875+Parsellol@users.noreply.github.com>
2022-07-09 17:19:55 +0200

    main menu scrolling

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -195,6 +195,12 @@ class MainMenuState extends MusicBeatState
 				changeItem(1);
 			}
 
+			if(FlxG.mouse.wheel != 0)
+				{
+					FlxG.sound.play(Paths.sound('scrollMenu'), 0.2);
+					changeItem(-FlxG.mouse.wheel);
+				}
+
 			if (controls.BACK)
 			{
 				selectedSomethin = true;
