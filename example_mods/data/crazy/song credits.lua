6555fb1b7539744cb5ce001da93e2db4b2fdb4fe
Isaiah Mods (Official) <118643764+IsaiahModsOfficial@users.noreply.github.com>
2022-12-10 20:37:34 -0800

    fixed cred

--- example_mods/data/crazy/song credits.lua
+++ example_mods/data/crazy/song credits.lua
@@ -1,5 +1,5 @@
 function onCreate() --COMPOSER NAME
-	makeLuaText('watermark', songName .. " | Banana B) ", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
+	makeLuaText('watermark', songName .. " | Maicon ", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
     setTextSize('watermark', 15);
 	addLuaText('watermark');
 end
