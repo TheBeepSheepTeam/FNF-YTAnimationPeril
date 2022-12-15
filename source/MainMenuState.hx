d0bc66f7b4417d5227daed683649b30327713660
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2022-12-15 17:32:26 -0500

    i rushed?!? bruhhh

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -160,7 +160,7 @@ class MainMenuState extends MusicBeatState
 		versionShitFunni.scrollFactor.set();
 		versionShitFunni.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShitFunni);
-		var versionShit,FlxText = new FlxText(12, FlxG.height - 24, 0, "Psych Engine v0.6.3 | Friday Night Funkin' v" + Application.current.meta.get('version'), 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "Psych Engine v0.6.3 | Friday Night Funkin' v" + Application.current.meta.get('version'), 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
