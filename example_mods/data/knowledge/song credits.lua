a93b6d82de304ae066165561f26d58c3645e8f7d
Isaiah Mods (Official) <118643764+IsaiahModsOfficial@users.noreply.github.com>
2022-12-15 21:30:48 -0800

    silly creders

---
+++ example_mods/data/knowledge/song credits.lua
@@ -0,0 +1,5 @@
+function onCreate() --COMPOSER NAME
+	makeLuaText('watermark', songName .. " | CharlesCatYT ", 0, -2, 540); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 15);
+	addLuaText('watermark');
+end
