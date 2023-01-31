5b6039fc9f2e2a0b6a852c1901224f07da0cac2a
Isaiah Mods (Official) <118643764+IsaiahModsOfficial@users.noreply.github.com>
2023-01-30 19:38:47 -0800

    bruh

--- example_mods/data/call/song credits.lua
+++
@@ -1,5 +0,0 @@
-function onCreate() --COMPOSER NAME
-	makeLuaText('watermark', songName .. " | Maicon ", 0, -2, 540); -- You can edit the created by Prisma Text just dont change anything else
-    setTextSize('watermark', 15);
-	addLuaText('watermark');
-end
